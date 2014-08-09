d181a8d5bf2a381e1459acebd12e8adfd39009e8
jamal <in_design7@hotmail.com>
2014-08-09 15:34:37 -0500

    Initial Commit

---
+++ International Calendar Widget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  International Calendar Widget
+//
+//  Created by Jamal Kharrat on 8/9/14.
+//  Copyright (c) 2014 Jamal Kharrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
